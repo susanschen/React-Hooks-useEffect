1e07edbbe54a9bee30b47241a5845bf1fcf674d7
Susan Chen <30938981+susanschen@users.noreply.github.com>
2019-09-02 18:14:19 -0400

    React Hooks useState

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <header>
+        <h1>Fetching Data from an API</h1>
+        <h2>With React Hooks useEffect</h2>
+      </header>
+
+      <div>
+        <p>You clicked {count} times </p>
+        <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
+          {" "}
+          Click me{" "}
+        </button>
+      </div>
+      <footer className="footer">
+        Tutorial from{" "}
+        <a href="https://www.youtube.com/watch?v=k0WnY0Hqe5c">Ben Awad</a>
+      </footer>
     </div>
   );
 }
